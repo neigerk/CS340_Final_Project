47e71e08a76416c9af83e0a294f6563b46415ba7
neigerk <neigerk@oregonstate.edu>
2020-11-30 21:48:17 -0800

    New order test

--- neworder.js
+++ neworder.js
@@ -2,14 +2,8 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    var handlebars = require('handlebars');
-    handlebars.registerHelper("setVar", function(varName, varValue, options) {
-        options.data.root[varName] = varValue;
-    });
-
-
     function getPizza(res, mysql, context, complete){
-        mysql.pool.query("SELECT pizzaID as pid, pizza_name, pizza_price FROM Pizzas", function(error, results, fields){
+        mysql.pool.query("SELECT pizzaID as pid, pizza_name FROM Pizzas", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -29,6 +23,21 @@ module.exports = function(){
             complete();
         });
     }
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    if(dd<10) 
+    {
+        dd='0'+dd;
+    } 
+
+    if(mm<10) 
+    {
+        mm='0'+mm;
+    } 
+    today = yyyy+'-'+mm+'-'+dd;
 
     router.get('/', function(req, res){
         var callbackCount = 0;
@@ -51,7 +60,7 @@ module.exports = function(){
         console.log(req.body)
         var mysql = req.app.get('mysql');
         var sql = "INSERT INTO Orders (order_price, date, CID, order_status) VALUES (?,?,?,?)";
-        var inserts = [req.body.first_name, req.body.last_name, req.body.cid, "ORDERED"];
+        var inserts = [req.body.total_price, today, req.body.cid, "ORDERED"];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 console.log(JSON.stringify(error))
@@ -62,6 +71,5 @@ module.exports = function(){
             }
         });
     });
-
     return router;
 }();
---
+++ public/updateorderprice.js
@@ -0,0 +1,38 @@
+function updatePrice(res, mysql) {
+    var pizza1 = document.getElementByID('p1').value;
+    var pizza2 = document.getElementByID('p2').value;
+    var pizza3 = document.getElementByID('p3').value;
+    var price1 = 0.0;
+    var price2 = 0.0;
+    var price3 = 0.0;
+    var total_price = document.getElementByID('totalPrice').value;
+
+    mysql.pool.query("SELECT pizza_price FROM Pizzas WHERE pizzaID = " + Number(pizza1), function(error, results){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+
+        price1 = results;
+    });
+
+    mysql.pool.query("SELECT pizza_price FROM Pizzas WHERE pizzaID = " + Number(pizza2), function(error, results){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+
+        price2 = results;
+    });
+
+    mysql.pool.query("SELECT pizza_price FROM Pizzas WHERE pizzaID = " + Number(pizza3), function(error, results){
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+
+        price3 = results;
+    });
+
+    document.getElementById('total_price').innerHTML = Number(total_price) + price3 + price2 + price1;
+}
