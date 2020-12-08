3bc72b3f2185566051579a91b32b95e622306094
neigerk <neigerk@oregonstate.edu>
2020-12-07 19:14:45 -0800

    an attempt

--- neworder.js
+++ neworder.js
@@ -119,7 +119,7 @@ module.exports = function(){
           res.write(JSON.stringify(error));
           res.end();
         }
-        context.price = results;
+        context.price = results[0];
         complete();
       })
     }
