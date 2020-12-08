01d9cca2e9342e140dd2b5df377766a29eecf6fb
neigerk <neigerk@oregonstate.edu>
2020-12-07 18:44:37 -0800

    Testing

--- public/updateorderprice.js
+++ public/updateorderprice.js
@@ -13,12 +13,24 @@ function updatePrice(){
   p1 = document.getElementById('pizza1');
   p2 = document.getElementById('pizza2');
   p3 = document.getElementById('pizza3');
-  console.log(getPrice(p1));
-  console.log(getPrice(p2));
-  console.log(getPrice(p3));
-  console.log(getPrice(p1.value));
-  console.log(getPrice(p2.value));
-  console.log(getPrice(p3.value));
+  if (p1.value !== ''){
+    console.log("p1.value:" + p1.value));
+    console.log("p1.response:" + getPrice(p1.value));
+  }
+  if (p2.value !== ''){
+    console.log("p2.value:" + p2.value));
+    console.log("p2.response:" + getPrice(p2.value));
+  }
+  if (p3.value !== ''){
+    console.log("p3.value:" + p3.value));
+    console.log("p3.response:" + getPrice(p3.value));
+  }
+  // console.log(getPrice(p1.value));
+  // console.log(getPrice(p2.value));
+  // console.log(getPrice(p3.value));
+  // console.log(getPrice(p1.value));
+  // console.log(getPrice(p2.value));
+  // console.log(getPrice(p3.value));
 }
 
 function getPrice(pizzaID){
