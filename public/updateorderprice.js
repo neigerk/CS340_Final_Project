3fae15cd80988e6741898d76a419d4226779a7a3
neigerk <neigerk@oregonstate.edu>
2020-12-07 18:48:13 -0800

    debugging

--- public/updateorderprice.js
+++ public/updateorderprice.js
@@ -43,6 +43,7 @@ function getPrice(pizzaID){
         var responseBody = event.target.response;
         alert("Error accessing pizza prices " + responseBody);
       } else {
+        p4 = event.target.response;
         return event.target.response;
       }
     });
