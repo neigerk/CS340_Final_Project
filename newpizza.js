05b24dea5693240b31d64e5cbe1e0d751ba45dfe
neigerk <neigerk@oregonstate.edu>
2020-12-07 23:35:02 -0800

    hope

--- newpizza.js
+++ newpizza.js
@@ -58,88 +58,169 @@ module.exports = function(){
             pid = results[0].pizzaID;
             console.log(results);
             console.log(pid);
+
+            if(req.body.topping1 == 'NULL'){
+                res.redirect('./');
+            }else{
+            var sql1 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+            var inserts1 = [pid, Number(req.body.topping1)];
+            sql1 = mysql.pool.query(sql1,inserts1,function(error, results, fields){
+                if(error){
+                    console.log(JSON.stringify(error))
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }//else{
+                 //   res.redirect('./');
+                //}
+            });
+            }
+
+            if(req.body.topping2 == 'NULL'){
+                res.redirect('./');
+            }else{
+            var sql2 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+            var inserts2 = [pid, Number(req.body.topping2)];
+            sql2 = mysql.pool.query(sql2,inserts2,function(error, results, fields){
+                if(error){
+                    console.log(JSON.stringify(error))
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }//else{
+                 //   res.redirect('./');
+                //}
+            });
+            }
+
+            if(req.body.topping3 == 'NULL'){
+                res.redirect('./');
+            }else{
+            var sql3 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+            var inserts3 = [pid, Number(req.body.topping3)];
+            sql3 = mysql.pool.query(sql3,inserts3,function(error, results, fields){
+                if(error){
+                    console.log(JSON.stringify(error))
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }//else{
+                 //   res.redirect('./');
+                //}
+            });
+            }
+
+            if(req.body.topping4 == 'NULL'){
+                res.redirect('./');
+            }else{
+            var sql4 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+            var inserts4 = [pid, Number(req.body.topping4)];
+            sql4 = mysql.pool.query(sql4,inserts4,function(error, results, fields){
+                if(error){
+                    console.log(JSON.stringify(error))
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }//else{
+                 //   res.redirect('./');
+                //}
+            });
+            }
+
+            if(req.body.topping5 == 'NULL'){
+                res.redirect('./');
+            }else{
+            var sql5 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+            var inserts5 = [pid, Number(req.body.topping5)];
+            sql5 = mysql.pool.query(sql5,inserts5,function(error, results, fields){
+                if(error){
+                    console.log(JSON.stringify(error))
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }//else{
+                 //   res.redirect('./');
+                //}
+            });
+            }
+
             }
         });
 
-        if(req.body.topping1 == 'NULL'){
-            res.redirect('./');
-        }else{
-        var sql1 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
-        var inserts1 = [pid, Number(req.body.topping1)];
-        sql1 = mysql.pool.query(sql1,inserts1,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }//else{
-             //   res.redirect('./');
-            //}
-        });
-        }
-
-        if(req.body.topping2 == 'NULL'){
-            res.redirect('./');
-        }else{
-        var sql2 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
-        var inserts2 = [pid, Number(req.body.topping2)];
-        sql2 = mysql.pool.query(sql2,inserts2,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }//else{
-             //   res.redirect('./');
-            //}
-        });
-        }
-
-        if(req.body.topping3 == 'NULL'){
-            res.redirect('./');
-        }else{
-        var sql3 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
-        var inserts3 = [pid, Number(req.body.topping3)];
-        sql3 = mysql.pool.query(sql3,inserts3,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }//else{
-             //   res.redirect('./');
-            //}
-        });
-        }
-
-        if(req.body.topping4 == 'NULL'){
-            res.redirect('./');
-        }else{
-        var sql4 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
-        var inserts4 = [pid, Number(req.body.topping4)];
-        sql4 = mysql.pool.query(sql4,inserts4,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }//else{
-             //   res.redirect('./');
-            //}
-        });
-        }
-
-        if(req.body.topping5 == 'NULL'){
-            res.redirect('./');
-        }else{
-        var sql5 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
-        var inserts5 = [pid, Number(req.body.topping5)];
-        sql5 = mysql.pool.query(sql5,inserts5,function(error, results, fields){
-            if(error){
-                console.log(JSON.stringify(error))
-                res.write(JSON.stringify(error));
-                res.end();
-            }//else{
-             //   res.redirect('./');
-            //}
-        });
-        }
+        // if(req.body.topping1 == 'NULL'){
+        //     res.redirect('./');
+        // }else{
+        // var sql1 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+        // var inserts1 = [pid, Number(req.body.topping1)];
+        // sql1 = mysql.pool.query(sql1,inserts1,function(error, results, fields){
+        //     if(error){
+        //         console.log(JSON.stringify(error))
+        //         res.write(JSON.stringify(error));
+        //         res.end();
+        //     }//else{
+        //      //   res.redirect('./');
+        //     //}
+        // });
+        // }
+        //
+        // if(req.body.topping2 == 'NULL'){
+        //     res.redirect('./');
+        // }else{
+        // var sql2 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+        // var inserts2 = [pid, Number(req.body.topping2)];
+        // sql2 = mysql.pool.query(sql2,inserts2,function(error, results, fields){
+        //     if(error){
+        //         console.log(JSON.stringify(error))
+        //         res.write(JSON.stringify(error));
+        //         res.end();
+        //     }//else{
+        //      //   res.redirect('./');
+        //     //}
+        // });
+        // }
+        //
+        // if(req.body.topping3 == 'NULL'){
+        //     res.redirect('./');
+        // }else{
+        // var sql3 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+        // var inserts3 = [pid, Number(req.body.topping3)];
+        // sql3 = mysql.pool.query(sql3,inserts3,function(error, results, fields){
+        //     if(error){
+        //         console.log(JSON.stringify(error))
+        //         res.write(JSON.stringify(error));
+        //         res.end();
+        //     }//else{
+        //      //   res.redirect('./');
+        //     //}
+        // });
+        // }
+        //
+        // if(req.body.topping4 == 'NULL'){
+        //     res.redirect('./');
+        // }else{
+        // var sql4 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+        // var inserts4 = [pid, Number(req.body.topping4)];
+        // sql4 = mysql.pool.query(sql4,inserts4,function(error, results, fields){
+        //     if(error){
+        //         console.log(JSON.stringify(error))
+        //         res.write(JSON.stringify(error));
+        //         res.end();
+        //     }//else{
+        //      //   res.redirect('./');
+        //     //}
+        // });
+        // }
+        //
+        // if(req.body.topping5 == 'NULL'){
+        //     res.redirect('./');
+        // }else{
+        // var sql5 = "INSERT INTO Pizzas_Toppings (pizzaID, toppingID) VALUES(?, ?)";
+        // var inserts5 = [pid, Number(req.body.topping5)];
+        // sql5 = mysql.pool.query(sql5,inserts5,function(error, results, fields){
+        //     if(error){
+        //         console.log(JSON.stringify(error))
+        //         res.write(JSON.stringify(error));
+        //         res.end();
+        //     }//else{
+        //      //   res.redirect('./');
+        //     //}
+        // });
+        // }
 
     });
 
